30e93c381ed2cc7d6a4540f72afa4ed931f680af
Gustavo Felix de Lima <felixdelimagustavo4@gmail.com>
2026-08-10 15:25:51 +0000

    Baima

---
+++ Bixia/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section class="Obscurus">
+        <div>Baihu <b class="Proverbius">- "O tigre desce a montanha; O dragão sobe aos céus"</b></div>
+      </section>
+    </>
+  )
+}
+
+export default App
