c115a0610d01a9473010970918a40ae089d03a97
Gustavo Felix de Lima <felixdelimagustavo4@gmail.com>
2026-09-22 11:31:10 +0000

    as

--- Bixia/src/App.jsx
+++ Bixia/src/App.jsx
@@ -19,9 +19,32 @@ function App() {
           <button>Artefatos</button>
         </div>
       </section>
-
+      <hr />
       <section className="Ignius">
         <h1>Arsenal</h1>
+        <div className="wrapper">
+          <div className="sidebar">
+            <button>Arcos</button>
+            <button>Facões</button>
+            <button>Espadas</button>
+            <button>Machados</button>
+            <button>Haste</button>
+            <button>Armaduras</button>
+          </div>
+          <div className="lista">
+            <button>DoYi</button>
+            <button>DoYi'Dou</button>
+            <button>DoFang</button>
+            <button>DoFang'ye</button>
+            <button>DoDou</button>
+            <button>DoDou'Dou</button>
+            <button>DoXibia</button>
+            <button>DoYi'Liao</button>
+            <button>Parit</button>
+            <button>Paritbon</button>
+            <button>Tokun</button>
+          </div>
+        </div>
       </section>
     </>
   )
